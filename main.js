078c26877ad400454a65d20bdb6ce5413b4e78d9
Mark Hoffmann <grizzo@gmail.com>
2024-02-06 19:36:08 -0800

    event detail proto

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 (function() {     // not really necessary (just to hide our variables from the outside scope)
-    
-    alert('freat job');
 
     
-    const accordion = document.getElementsByClassName("accordion");     // the .accordion buttons (no need for panels, we can get them using nextElementSibling)
+    const accordion = document.getElementsByClassName("faq_button");     // the .accordion buttons (no need for panels, we can get them using nextElementSibling)
     let current = -1;                                                   // the index of the current active accordion element (-1 indicate that currently no element is active)
 
     for (let i = 0; i < accordion.length; i++) {
-        accordion[i].addEventListener('click', function() {             // when clicking a .accordion element
+        accordion[i].addEventListener('click', function(e) {    
+            e.preventDefault();         // when clicking a .accordion element
             if (i !== current && current !== -1) {                      // if this is not the currently active element (i !== current), and if there is a currently active element (current !== -1)
                 accordion[current].classList.remove('active');          // then desactivate the currently active element
                 accordion[current].nextElementSibling.classList.remove('panel-open'); // ...
